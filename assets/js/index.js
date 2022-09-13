ecd59f378080522d88c3a9ade350921129230fbf
Rawane Al-Zohbi <rawane.z@outlook.com>
2022-09-14 00:09:26 +0300

    first commit by me

---
+++ assets/js/index.js
@@ -0,0 +1,86 @@
+
+var services = document.getElementById('Services');
+var about=document.getElementById('rawane');
+
+var Orig_content=`<div class="col-sm-4">
+<div class="main-service text-right">
+    <div class="rectangle">
+        <i class="fa fa-calendar" aria-hidden="true"></i>
+    </div><!-- /.rectangle -->
+    <h3>svc</h3>
+    <p >description</p>
+</div><!-- /.main-service -->`
+
+var original=` <div class="col-sm-3 col-xs-6">
+<div class="feature-about">
+    <div class="medium-rectangle rectangle">
+        <i class="fa fa-laptop" aria-hidden="true"></i>
+    </div><!-- /.rectangle -->
+    <h3>title</h3>
+    <p>explication</p>
+</div><!-- /.feature-about -->
+</div>                    `
+
+/*<div class="col-sm-3 col-xs-6">
+                        <div class="feature-about">
+                            <div class="medium-rectangle rectangle">
+                                <i class="fa fa-code" aria-hidden="true"></i>
+                            </div><!-- /.rectangle -->
+                            <h3>Web development</h3>
+                            <p>The web development process involves taking the graphical elements defined in the design process and coding them into a custom theme.</p>
+                        </div><!-- /.feature-about -->
+                    </div>                    
+                    <div class="col-sm-3 col-xs-6">
+                        <div class="feature-about">
+                            <div class="medium-rectangle rectangle">
+                                <i class="fa fa-search" aria-hidden="true"></i>
+                            </div><!-- /.rectangle -->
+                            <h3>SEO OPTIMIZATION</h3>
+                            <p>Go farther than you thought you could. With us you can go farther then ever before. Be in top results of searches.</p>
+                        </div><!-- /.feature-about -->
+                    </div>                    
+                    <div class="col-sm-3 col-xs-6">
+                        <div class="feature-about">
+                            <div class="medium-rectangle rectangle">
+                                <i class="fa fa-twitter" aria-hidden="true"></i>
+                            </div><!-- /.rectangle -->
+                            <h3>SOCIAL MEDIA</h3>
+                            <p>It's important to keep your brand consistent and recognizable across all platforms</p>
+                        </div><!-- /.feature-about -->
+                    </div>
+                </div>
+             </div>*/
+fetch('https://shrouded-lowlands-59927.herokuapp.com/services')
+  .then(response => response.json())
+  .then(json => {
+    json.services.forEach(element => {
+        debugger;
+     var content = Orig_Content;
+     content = content.replace('svc',element.A);
+     console.log(content)
+     content = content.replace('description',element.B);
+     console.log(content)
+        
+     var service = document.createElement('div')
+     service.innerHTML = content;
+     service.className = 'col-lg-4 col-md-6 icon-box';
+     services.appendChild(service)
+        
+    });
+  })
+
+  fetch('https://shrouded-lowlands-59927.herokuapp.com/myself')
+  .then(response => response.json())
+  .then(json => {
+    json.Rawane.forEach(element => {
+     var content = original;
+     content = content.replace('title',element.A);
+     content = content.replace('explication',element.B);
+        
+     var abt = document.createElement('div')
+     abt.innerHTML = content;
+     abt.className = 'col-lg-4 col-md-6 icon-box';
+     about.appendChild(service)
+        
+    });
+  })
